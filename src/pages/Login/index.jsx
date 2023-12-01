22a0c33f7fc1540cec9c44236979e27ec6c5feac
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-12-01 10:15:16 +0400

    create a login component

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import logo from '../../assets/images/svgviewer-output.png';
+import { Text } from '../../components/Text';
+import styles from './styles.module.scss';
+
 export const Login = () => {
-  return <h2>Log in to Boost Points</h2>;
+  return (
+    <div className={styles.login}>
+      <div>
+        <img src={logo} alt="logo" />
+        <Text type="h4" className={styles.heading}>
+          Log in
+        </Text>
+      </div>
+    </div>
+  );
 };
