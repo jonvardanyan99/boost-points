23546a5c5703f422fddc9ce48ce240a932e0e13b
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-12-08 10:41:19 +0400

    install react spinners and create Loader component for showing loading

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { React } from 'react';
+import BeatLoader from 'react-spinners/BeatLoader';
+
+export const Loader = ({
+  color = 'var(--accent-color)',
+  size = 30,
+  cssOverride = {},
+  speedMultiplier,
+}) => {
+  const css = { display: 'flex', justifyContent: 'center', ...cssOverride };
+
+  return (
+    <BeatLoader color={color} size={size} cssOverride={css} speedMultiplier={speedMultiplier} />
+  );
+};
+
+Loader.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.number,
+  cssOverride: PropTypes.shape({}),
+  speedMultiplier: PropTypes.number,
+};
