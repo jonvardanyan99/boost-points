325dc6939692230b285d7dab7b1db9a06fed5f46
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-12-27 14:13:14 +0400

    include document property

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { reportWebVitals } from './reportWebVitals';
 
-const root = ReactDOM.createRoot(global.getElementById('root'));
+const root = ReactDOM.createRoot(global.document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
