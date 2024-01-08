f289829bb9e31c2f62d1511a7610aca69ee0de74
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-01-08 12:45:38 +0400

    create validation schema for the OTP

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -5,3 +5,9 @@ export const phoneNumberSchema = z
   .min(1, 'This field is required')
   .length(10, 'Invalid phone number')
   .regex(/^[0-9]+$/, 'Invalid phone number');
+
+export const otpSchema = z
+  .string()
+  .min(1, 'This field is required')
+  .length(4, 'Invalid OTP')
+  .regex(/^[0-9]+$/, 'Invalid OTP');
