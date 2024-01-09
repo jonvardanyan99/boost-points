78f09a238c80f72e092f81606a8a7b98996d448e
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-01-09 12:43:19 +0400

    create constant variables for the texts

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,13 +1,17 @@
 import { z } from 'zod';
 
+const FIELD_REQUIRED = 'This field is required';
+const INVALID_PHONE_NUMBER = 'Invalid phone number';
+const INVALID_OTP = 'Invalid OTP';
+
 export const phoneNumberSchema = z
   .string()
-  .min(1, 'This field is required')
-  .length(10, 'Invalid phone number')
-  .regex(/^[0-9]+$/, 'Invalid phone number');
+  .min(1, FIELD_REQUIRED)
+  .length(10, INVALID_PHONE_NUMBER)
+  .regex(/^[0-9]+$/, INVALID_PHONE_NUMBER);
 
 export const otpSchema = z
   .string()
-  .min(1, 'This field is required')
-  .length(4, 'Invalid OTP')
-  .regex(/^[0-9]+$/, 'Invalid OTP');
+  .min(1, FIELD_REQUIRED)
+  .length(4, INVALID_OTP)
+  .regex(/^[0-9]+$/, INVALID_OTP);
