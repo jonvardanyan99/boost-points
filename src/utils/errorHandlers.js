bbea6d4831688ec904e0335c28b16123f96bd379
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-01-09 12:38:59 +0400

    add proper error handling logic

--- src/utils/errorHandlers.js
+++ src/utils/errorHandlers.js
@@ -1,7 +1,9 @@
 export const handleApiError = (error, setMessage, key) => {
-  if (error.response.status === 429) {
+  if (typeof error.response.data.detail === 'string') {
     setMessage(error.response.data.detail);
-  } else {
+  } else if (Array.isArray(error.response.data.detail)) {
     setMessage(error.response.data.detail[0][key][0]);
+  } else {
+    setMessage(error.response.data.detail[key][0]);
   }
 };
