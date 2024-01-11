a97a82178bbad4a76ca05c7965bd476da2175dc5
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-01-11 14:44:03 +0400

    provide the store and create verification route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.scss';
 
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Login } from './pages/Login';
+import { Verification } from './pages/Verification';
+import { store } from './store';
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/boost-points">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/boost-points">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/verification" element={<Verification />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
