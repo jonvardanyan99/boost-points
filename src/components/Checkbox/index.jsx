4a01ae260e47b4b2303d4f3d39466cee4a6b2085
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-01-26 13:21:42 +0400

    create Checkbox component

---
+++ src/components/Checkbox/index.jsx
@@ -0,0 +1,39 @@
+import checkbox from 'assets/images/checkbox.svg';
+import checkboxActive from 'assets/images/checkbox-active.svg';
+import classNames from 'classnames';
+import { Text } from 'components/Text';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export const Checkbox = ({ className, checked, onChange, label }) => {
+  const toggleChecked = () => {
+    onChange(prevValue => !prevValue);
+  };
+
+  return (
+    <div className={classNames(styles.button, className)}>
+      <button type="button" id={label} onClick={toggleChecked}>
+        <img
+          className={styles.button__checkbox}
+          src={checked ? checkboxActive : checkbox}
+          alt="checkbox"
+        />
+      </button>
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor={label}>
+        <Text type="p3" className={styles.button__text}>
+          {label}
+        </Text>
+      </label>
+    </div>
+  );
+};
+
+Checkbox.propTypes = {
+  className: PropTypes.string,
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
