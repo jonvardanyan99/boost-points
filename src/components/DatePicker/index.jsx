079913a907e92526a2856511c52af675cbc48960
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-02-01 14:16:34 +0400

    create date picker component

---
+++ src/components/DatePicker/index.jsx
@@ -0,0 +1,91 @@
+import 'react-datepicker/dist/react-datepicker.css';
+
+import calendar from 'assets/images/calendar-2.svg';
+import downArrow from 'assets/images/down-arrow.svg';
+import classNames from 'classnames';
+import { Text } from 'components/Text';
+import format from 'date-fns/format';
+import PropTypes from 'prop-types';
+import React, { useEffect, useRef, useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+
+import styles from './styles.module.scss';
+
+export const DatePicker = ({ className, placeholder, value, onChange, label }) => {
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
+  const datePickerRef = useRef(null);
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (datePickerVisible) {
+      const handleClickOutside = event => {
+        if (!datePickerRef.current.contains(event.target)) {
+          setDatePickerVisible(false);
+        }
+      };
+
+      global.addEventListener('click', handleClickOutside);
+
+      return () => global.removeEventListener('click', handleClickOutside);
+    }
+  }, [datePickerVisible]);
+
+  const getDayClassName = date => {
+    const currentDay =
+      value?.getFullYear() === date.getFullYear() &&
+      value?.getMonth() === date.getMonth() &&
+      value?.getDate() === date.getDate();
+
+    if (currentDay) {
+      return styles['day--selected'];
+    }
+
+    return styles['day--keyboard-selected'];
+  };
+
+  const toggleDatePickerVisible = () => {
+    setDatePickerVisible(prevValue => !prevValue);
+  };
+
+  return (
+    <div ref={datePickerRef} className={classNames(styles['date-picker-container'], className)}>
+      <label htmlFor={label}>
+        <Text type="p4" className={styles['date-picker-container__label-text']}>
+          {label}
+        </Text>
+      </label>
+      <button
+        type="button"
+        id={label}
+        className={classNames(styles['main-button'], {
+          [styles['main-button--active']]: datePickerVisible,
+        })}
+        onClick={toggleDatePickerVisible}
+      >
+        <div>
+          <img src={calendar} alt="calendar" />
+          <Text type="p4">{value ? format(value, 'dd/MM/y') : placeholder}</Text>
+        </div>
+        <img src={downArrow} alt="down-arrow" />
+      </button>
+      {datePickerVisible && (
+        <ReactDatePicker
+          calendarClassName={styles['date-picker']}
+          dayClassName={getDayClassName}
+          selected={value}
+          onChange={date => onChange(date)}
+          onSelect={() => setDatePickerVisible(false)}
+          inline
+        />
+      )}
+    </div>
+  );
+};
+
+DatePicker.propTypes = {
+  className: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.shape({}),
+  onChange: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
