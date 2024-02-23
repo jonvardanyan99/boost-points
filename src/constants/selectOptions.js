a74829ef7dd890b1ef0049cd04e5dfe5fb995deb
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-02-23 13:22:25 +0400

    create constant options for dropdown selects

---
+++ src/constants/selectOptions.js
@@ -0,0 +1,23 @@
+export const GENDER_OPTIONS = [
+  { label: 'Male', value: 'Male' },
+  { label: 'Female', value: 'Female' },
+];
+
+export const STATES_OPTIONS = [
+  { label: 'Australian Capital Territory', value: 'ACT' },
+  { label: 'New South Wales', value: 'NSW' },
+  { label: 'Northern Territory', value: 'NT' },
+  { label: 'New Zealand', value: 'NZ' },
+  { label: 'Overseas', value: 'OS' },
+  { label: 'Other State or Territory of Australia', value: 'OTH' },
+  { label: 'Queensland', value: 'QLD' },
+  { label: 'South Australia', value: 'SA' },
+  { label: 'Tasmania', value: 'TAS' },
+  { label: 'Victoria', value: 'VIC' },
+  { label: 'Western Australia', value: 'WA' },
+];
+
+export const COUNTRY_CODE_OPTIONS = [
+  { label: 'AU', value: 'AU' },
+  { label: 'NZ', value: 'NZ' },
+];
