96c5ec9d2efd159bb353559e80826d088b50f463
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-03-19 10:09:37 +0400

    add validation form schema for the create account page and some configure

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -3,6 +3,8 @@ import { z } from 'zod';
 const FIELD_REQUIRED = 'This field is required';
 const INVALID_PHONE_NUMBER = 'Invalid phone number';
 const INVALID_OTP = 'Invalid OTP';
+const INVALID_EMAIL = 'Invalid email';
+const INVALID_DATE = 'Invalid date';
 
 export const loginFormSchema = z.object({
   phoneNumber: z
@@ -20,6 +22,8 @@ export const verificationFormSchema = z.object({
 
 export const confirmAddressFormSchema = z.object({
   propertyName: z.string({ required_error: FIELD_REQUIRED }),
+  unitNumber: z.string().optional(),
+  streetNumber: z.string().optional(),
   streetName: z.string({ required_error: FIELD_REQUIRED }),
   streetSuffix: z.string({ required_error: FIELD_REQUIRED }),
   suburb: z.string({ required_error: FIELD_REQUIRED }),
@@ -36,4 +40,41 @@ export const confirmAddressFormSchema = z.object({
     .refine(item => item !== null, {
       message: FIELD_REQUIRED,
     }),
+});
+
+export const createAccountFormSchema = z.object({
+  firstName: z.string({ required_error: FIELD_REQUIRED }),
+  middleName: z.string().optional(),
+  surname: z.string({ required_error: FIELD_REQUIRED }),
+  birthDate: z
+    .date()
+    .nullable()
+    .superRefine((item, ctx) => {
+      if (item === null) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: FIELD_REQUIRED,
+        });
+      }
+
+      const currentDate = new Date();
+
+      if (item >= currentDate) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.invalid_date,
+          message: INVALID_DATE,
+        });
+      }
+    }),
+  gender: z
+    .object({ label: z.string(), value: z.string() })
+    .nullable()
+    .refine(item => item !== null, {
+      message: FIELD_REQUIRED,
+    }),
+  email: z.string({ required_error: FIELD_REQUIRED }).email(INVALID_EMAIL),
+  residentialAddress: confirmAddressFormSchema.nullable().refine(item => item !== null, {
+    message: FIELD_REQUIRED,
+  }),
+  previousAddress: confirmAddressFormSchema.nullable(),
 });
