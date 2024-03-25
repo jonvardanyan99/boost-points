f688b7a8eac402565ca7660b258660090a88f760
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-03-25 11:55:52 +0400

    delete unnecessary function

--- src/utils/errorHandlers.js
+++ src/utils/errorHandlers.js
@@ -1,20 +1,3 @@
-export const handleApiError = (error, setError, keys) => {
-  const { detail } = error.response.data;
-
-  if (typeof detail === 'string') {
-    setError(keys[0], detail);
-  } else {
-    const errorDetails = Array.isArray(detail) ? detail[0] : detail;
-
-    // eslint-disable-next-line array-callback-return
-    Object.keys(errorDetails).map(apiKey => {
-      if (keys.includes(apiKey)) {
-        setError(apiKey, errorDetails[apiKey][0]);
-      }
-    });
-  }
-};
-
 export const getFormikError = (formik, key) => {
   if (formik.errors[key] && formik.touched[key]) {
     return formik.errors[key];
