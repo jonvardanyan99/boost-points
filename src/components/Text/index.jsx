11710ba802ddb1c5e7c2ea06727808ce0df49699
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-04-01 14:17:53 +0400

    change the order of the prop and reduced the logic of the code

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-export const Text = ({ type, children, className, fontWeight }) => {
+export const Text = ({ type, children, fontWeight, className }) => {
   if (type === 'h1') {
     return <h1 className={classNames(styles.h1, className)}>{children}</h1>;
   }
@@ -29,61 +29,17 @@ export const Text = ({ type, children, className, fontWeight }) => {
     return <h6 className={classNames(styles.h6, className)}>{children}</h6>;
   }
 
-  if (type === 'p1') {
-    return (
-      <p className={classNames(styles.p1, className, styles[`p1--weight-${fontWeight}`])}>
-        {children}
-      </p>
-    );
-  }
-
-  if (type === 'p2') {
-    return (
-      <p className={classNames(styles.p2, className, styles[`p2--weight-${fontWeight}`])}>
-        {children}
-      </p>
-    );
-  }
-
-  if (type === 'p3') {
-    return (
-      <p className={classNames(styles.p3, className, styles[`p3--weight-${fontWeight}`])}>
-        {children}
-      </p>
-    );
-  }
-
-  if (type === 'p4') {
-    return (
-      <p className={classNames(styles.p4, className, styles[`p4--weight-${fontWeight}`])}>
-        {children}
-      </p>
-    );
-  }
-
-  if (type === 'p5') {
-    return (
-      <p className={classNames(styles.p5, className, styles[`p5--weight-${fontWeight}`])}>
-        {children}
-      </p>
-    );
-  }
-
-  if (type === 'p6') {
-    return (
-      <p className={classNames(styles.p6, className, styles[`p6--weight-${fontWeight}`])}>
-        {children}
-      </p>
-    );
-  }
-
-  return null;
+  return (
+    <p className={classNames(styles[type], styles[`${type}--weight-${fontWeight}`], className)}>
+      {children}
+    </p>
+  );
 };
 
 Text.propTypes = {
   type: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6'])
     .isRequired,
   children: PropTypes.string.isRequired,
-  className: PropTypes.string,
   fontWeight: PropTypes.oneOf([400, 500, 600, 700]),
+  className: PropTypes.string,
 };
