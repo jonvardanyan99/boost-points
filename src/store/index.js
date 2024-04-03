0ff788c5bc071303406eb649965ccf635a870c06
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-04-03 10:18:06 +0400

    redux persist configure and create an action that resets the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,34 @@
 import { combineReducers, createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+import { RESET_STORE } from './reducers/app/actions';
 import { authReducer } from './reducers/auth';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   auth: authReducer,
 });
 
+const rootReducer = (state, action) => {
+  if (action.type === RESET_STORE) {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  rootReducer,
+  persistedRootReducer,
   // eslint-disable-next-line no-underscore-dangle, no-undef
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+export const persistor = persistStore(store);
