49505f9ce5e85f49b54b2aba502ba67419e14079
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-04-05 10:25:49 +0400

    add the me getting api and the response interceptor

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
 import { API_URL } from 'constants/env';
 import { store } from 'store';
+import { setTokens } from 'store/reducers/auth/actions';
+
+const REFRESH_TOKEN_URL = '/api/v1/consumers/token/refresh';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
 });
+
+export const API = {
+  login: data => axiosInstance.post('/api/v1/consumers/otp/send', data),
+  verifyPhoneNumber: data => axiosInstance.post('/api/v1/consumers/otp/check', data),
+  refreshToken: data => axiosInstance.post(REFRESH_TOKEN_URL, data),
+  getAccount: () => axiosInstance.get('/api/v1/consumers/me'),
+  createAccount: data => axiosInstance.post('/api/v1/consumers/me', data),
+};
 
 axiosInstance.interceptors.request.use(config => {
   const state = store.getState();
@@ -17,8 +28,31 @@ axiosInstance.interceptors.request.use(config => {
   return config;
 });
 
-export const API = {
-  login: data => axiosInstance.post('/api/v1/consumers/otp/send', data),
-  verifyPhoneNumber: data => axiosInstance.post('/api/v1/consumers/otp/check', data),
-  createAccount: data => axiosInstance.post('/api/v1/consumers/me', data),
-};
+axiosInstance.interceptors.response.use(
+  response => response,
+  async error => {
+    if (error.response.status === 401 && error.response.config.url !== REFRESH_TOKEN_URL) {
+      try {
+        const state = store.getState();
+        const refreshToken = state.auth.refreshToken;
+
+        const response = await API.refreshToken({
+          credentials: refreshToken,
+        });
+
+        store.dispatch(
+          setTokens({
+            accessToken: response.data.accessToken,
+            refreshToken: response.data.refreshToken,
+          }),
+        );
+      } catch (e) {
+        return Promise.reject(e);
+      }
+
+      return axiosInstance(error.config);
+    }
+
+    return Promise.reject(error);
+  },
+);
