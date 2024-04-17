1af0e3a0a751e5a18266d4b3b25fd4c737f306c1
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-04-17 10:05:42 +0400

    add routing logic related to whether the account has been created

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -6,10 +6,13 @@ import { Verification } from 'pages/Verification';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { selectIsAuthenticated } from 'store/reducers/auth/selectors';
+import { selectAccount, selectIsAuthenticated } from 'store/reducers/user/selectors';
 
 export const Router = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated);
+  const account = useSelector(selectAccount);
+
+  const hasAccountCreated = account?.isNew && !!account.firstName;
 
   return (
     <BrowserRouter basename="/boost-points">
@@ -19,8 +22,18 @@ export const Router = () => {
           <Route path={ROUTES.VERIFICATION} element={<Verification />} />
         </Route>
         <Route element={isAuthenticated ? undefined : <Navigate to={ROUTES.LOGIN} />}>
-          <Route path={ROUTES.CREATE_ACCOUNT} element={<CreateAccount />} />
-          <Route path={ROUTES.IDENTIFICATION} element={<Identification />} />
+          <Route
+            path={ROUTES.CREATE_ACCOUNT}
+            element={
+              hasAccountCreated ? <Navigate to={ROUTES.IDENTIFICATION} /> : <CreateAccount />
+            }
+          />
+          <Route
+            path={ROUTES.IDENTIFICATION}
+            element={
+              hasAccountCreated ? <Identification /> : <Navigate to={ROUTES.CREATE_ACCOUNT} />
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
