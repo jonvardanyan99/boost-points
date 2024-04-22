3cf5cf50be60e8c8d44a7dfbf884c6ffc7ac215c
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-04-22 14:40:17 +0400

    add api request sending and handling logic

--- src/pages/Identification/index.jsx
+++ src/pages/Identification/index.jsx
@@ -3,18 +3,46 @@ import { Button } from 'components/Button';
 import { Dropdown } from 'components/Dropdown';
 import { Input } from 'components/Input';
 import { Text } from 'components/Text';
-import { DOCUMENT_TYPE_OPTIONS, STATES_OPTIONS } from 'constants/selectOptions';
+import { DOCUMENT_TYPE_OPTIONS, STATE_OPTIONS } from 'constants/selectOptions';
 import { useFormik } from 'formik';
+import { useErrorHandler } from 'hooks/useErrorHandler';
+import { useMutation } from 'hooks/useMutation';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { API } from 'services/api';
+import { setIsNew } from 'store/reducers/user/actions';
+import { getFormikError } from 'utils/errorHandlers';
+import { identificationFormSchema } from 'utils/validators';
+import { toFormikValidationSchema } from 'zod-formik-adapter';
 
 import styles from './styles.module.scss';
 
 export const Identification = () => {
+  const dispatch = useDispatch();
+  const [addId, { loading }] = useMutation(API.addId);
+  const { handleApiError, snackbar } = useErrorHandler();
+
   const formik = useFormik({
     initialValues: {
       documentType: null,
-      issueState: null,
-      licenceNo: '',
+      state: null,
+      number: '',
+    },
+    validationSchema: toFormikValidationSchema(identificationFormSchema),
+    onSubmit: async values => {
+      try {
+        await addId({
+          documentType: values.documentType.value,
+          documentData: {
+            number: values.number,
+            state: values.state.value,
+          },
+        });
+
+        dispatch(setIsNew());
+      } catch (error) {
+        handleApiError(error, formik.setFieldError, ['documentType', 'state', 'number']);
+      }
     },
   });
 
@@ -37,32 +65,37 @@ export const Identification = () => {
             onBlur={() => formik.setFieldTouched('documentType', true, true)}
             options={DOCUMENT_TYPE_OPTIONS}
             label="Document type"
+            error={getFormikError(formik, 'documentType')}
           />
           <Dropdown
             className={styles.identification__field}
             placeholder="Select State"
-            selectedOption={formik.values.issueState}
-            onChange={option => formik.setFieldValue('issueState', option, true)}
-            onBlur={() => formik.setFieldTouched('issueState', true, true)}
-            options={STATES_OPTIONS}
+            selectedOption={formik.values.state}
+            onChange={option => formik.setFieldValue('state', option, true)}
+            onBlur={() => formik.setFieldTouched('state', true, true)}
+            options={STATE_OPTIONS}
             label="State of issue"
+            error={getFormikError(formik, 'state')}
           />
           <Input
             className={styles.identification__field}
             placeholder="249004225"
-            name="licenceNo"
-            value={formik.values.licenceNo}
+            name="number"
+            value={formik.values.number}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             label="Licence no"
+            error={getFormikError(formik, 'number')}
           />
           <Button
             className={styles['identification__main-button']}
             title="Continue"
-            onClick={() => {}}
+            onClick={formik.handleSubmit}
+            loading={loading}
           />
         </div>
       </div>
+      {snackbar}
     </div>
   );
 };
