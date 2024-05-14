a989680d989e68d1e676d39868670aa2666be7d6
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-05-14 10:36:14 +0400

    create a data url to file converter function

---
+++ src/utils/helpers.js
@@ -0,0 +1,6 @@
+export const dataUrlToFile = async (dataUrl, fileName) => {
+  const res = await fetch(dataUrl);
+  const blob = await res.blob();
+
+  return new File([blob], fileName, { type: 'image/png' });
+};
