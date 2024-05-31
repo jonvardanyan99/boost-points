48b575ac08227f06f8cb8179d2144b63fd165955
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-05-31 14:48:36 +0400

    handle unexpected errors

--- src/hooks/useErrorHandler.jsx
+++ src/hooks/useErrorHandler.jsx
@@ -1,6 +1,8 @@
 import { Snackbar } from 'components/Snackbar';
 import React, { useState } from 'react';
 
+const UNEXPECTED_ERROR = 'An unexpected error occurred.';
+
 export const useErrorHandler = () => {
   const [snackbarText, setSnackbarText] = useState('');
 
@@ -9,21 +11,25 @@ export const useErrorHandler = () => {
   };
 
   const handleApiError = (error, setError = () => {}, keys = []) => {
-    const { detail } = error.response.data;
+    if (error.response && error.response.data) {
+      const { detail } = error.response.data;
 
-    if (typeof detail === 'string') {
-      setSnackbarText(detail);
+      if (typeof detail === 'string') {
+        setSnackbarText(detail);
+      } else {
+        const errorDetails = Array.isArray(detail) ? detail[0] : detail;
+
+        // eslint-disable-next-line array-callback-return
+        Object.keys(errorDetails).forEach(apiKey => {
+          if (keys.includes(apiKey)) {
+            setError(apiKey, errorDetails[apiKey][0]);
+          } else {
+            setSnackbarText(`${apiKey}: ${errorDetails[Object.keys(errorDetails)[0]][0]}`);
+          }
+        });
+      }
     } else {
-      const errorDetails = Array.isArray(detail) ? detail[0] : detail;
-
-      // eslint-disable-next-line array-callback-return
-      Object.keys(errorDetails).forEach(apiKey => {
-        if (keys.includes(apiKey)) {
-          setError(apiKey, errorDetails[apiKey][0]);
-        } else {
-          setSnackbarText(`${apiKey}: ${errorDetails[Object.keys(errorDetails)[0]][0]}`);
-        }
-      });
+      setSnackbarText(UNEXPECTED_ERROR);
     }
   };
 
