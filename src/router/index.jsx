95357c4f356a361440e07e2f3ac5c412bd59339e
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-06-03 14:59:52 +0400

    add the wrapper component, add some routes and improve the code

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,9 +1,11 @@
+import { Page } from 'components/Page';
 import { ROUTES } from 'constants/routes';
 import { ConsentForm } from 'pages/ConsentForm';
 import { CreateAccount } from 'pages/CreateAccount';
 import { Dashboard } from 'pages/Dashboard';
 import { Identification } from 'pages/Identification';
 import { Login } from 'pages/Login';
+import { Report } from 'pages/Report';
 import { Verification } from 'pages/Verification';
 import React from 'react';
 import { useSelector } from 'react-redux';
@@ -14,44 +16,49 @@ export const Router = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const account = useSelector(selectAccount);
 
-  const isNew = account?.data?.isNew;
-  const hasAccountCreated = isNew && !!account.data.firstName;
-  const hasConsentFormSigned = !!account?.isConsentFormSigned;
+  const isNew = account?.data.isNew;
+  const isAccountCreated = isNew && !!account.data.firstName;
+  const isConsentFormSigned = !!account?.isConsentFormSigned;
 
   return (
     <BrowserRouter basename="/boost-points">
-      <Routes>
-        <Route element={isAuthenticated ? <Navigate to={ROUTES.CREATE_ACCOUNT} /> : undefined}>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.VERIFICATION} element={<Verification />} />
-        </Route>
-        <Route element={isAuthenticated ? undefined : <Navigate to={ROUTES.LOGIN} />}>
-          <Route element={isNew ? undefined : <Navigate to={ROUTES.CONSENT_FORM} />}>
-            <Route
-              path={ROUTES.CREATE_ACCOUNT}
-              element={
-                hasAccountCreated ? <Navigate to={ROUTES.IDENTIFICATION} /> : <CreateAccount />
-              }
-            />
-            <Route
-              path={ROUTES.IDENTIFICATION}
-              element={
-                hasAccountCreated ? <Identification /> : <Navigate to={ROUTES.CREATE_ACCOUNT} />
-              }
-            />
+      <Page>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.LOGIN} />} />
+          <Route element={isAuthenticated ? <Navigate to={ROUTES.CREATE_ACCOUNT} /> : undefined}>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.VERIFICATION} element={<Verification />} />
           </Route>
-          <Route element={isNew ? <Navigate to={ROUTES.CREATE_ACCOUNT} /> : undefined}>
-            <Route
-              path={ROUTES.CONSENT_FORM}
-              element={hasConsentFormSigned ? <Navigate to={ROUTES.DASHBOARD} /> : <ConsentForm />}
-            />
-            <Route
-              path={ROUTES.DASHBOARD}
-              element={hasConsentFormSigned ? <Dashboard /> : <Navigate to={ROUTES.CONSENT_FORM} />}
-            />
+          <Route element={isAuthenticated ? undefined : <Navigate to={ROUTES.LOGIN} />}>
+            <Route element={isNew ? undefined : <Navigate to={ROUTES.CONSENT_FORM} />}>
+              <Route
+                path={ROUTES.CREATE_ACCOUNT}
+                element={
+                  isAccountCreated ? <Navigate to={ROUTES.IDENTIFICATION} /> : <CreateAccount />
+                }
+              />
+              <Route
+                path={ROUTES.IDENTIFICATION}
+                element={
+                  isAccountCreated ? <Identification /> : <Navigate to={ROUTES.CREATE_ACCOUNT} />
+                }
+              />
+            </Route>
+            <Route element={isNew ? <Navigate to={ROUTES.CREATE_ACCOUNT} /> : undefined}>
+              <Route
+                path={ROUTES.CONSENT_FORM}
+                element={isConsentFormSigned ? <Navigate to={ROUTES.DASHBOARD} /> : <ConsentForm />}
+              />
+              <Route
+                element={isConsentFormSigned ? undefined : <Navigate to={ROUTES.CONSENT_FORM} />}
+              >
+                <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+                <Route path={ROUTES.REPORT} element={<Report />} />
+              </Route>
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Page>
     </BrowserRouter>
   );
 };
