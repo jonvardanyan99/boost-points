7f2d310c5b9f487d92423ab51def29b77c25720b
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-06-13 13:19:44 +0400

    the header and the menu modal usage

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,29 +1,32 @@
-import menu from 'assets/icons/menu.svg';
-import logo from 'assets/images/logo-white.svg';
 import classNames from 'classnames';
+import { Header } from 'components/Header';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectAccount } from 'store/reducers/user/selectors';
 
+import { MenuModal } from './components/MenuModal';
 import styles from './styles.module.scss';
 
 export const Page = ({ children }) => {
   const account = useSelector(selectAccount);
+  const [menuModalVisible, setMenuModalVisible] = useState(false);
 
   const isConsentFormSigned = !!account?.isConsentFormSigned;
 
+  const openMenuModal = () => {
+    setMenuModalVisible(true);
+  };
+
+  const closeMenuModal = () => {
+    setMenuModalVisible(false);
+  };
+
   return (
     <div className={classNames(styles.page, { [styles['page--signed']]: isConsentFormSigned })}>
-      {isConsentFormSigned && (
-        <header className={styles.page__header}>
-          <img src={logo} alt="logo" />
-          <button type="button" className={styles['page__menu-wrapper']}>
-            <img src={menu} alt="menu-icon" />
-          </button>
-        </header>
-      )}
+      {isConsentFormSigned && <Header icon="menu" onClick={openMenuModal} />}
       {children}
+      <MenuModal visible={menuModalVisible} onClose={closeMenuModal} account={account || {}} />
     </div>
   );
 };
