1a9ed3b2c794657b51660afa0f3244716bc0cdb2
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-06-21 14:19:35 +0400

    simplify the design code

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -10,30 +10,28 @@ import styles from './styles.module.scss';
 export const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
-      <div className={styles.dashboard__content}>
-        <div className={styles['dashboard__credit-score-container']}>
-          <Text type="h6">Credit Scores</Text>
-          <CreditScore
-            className={styles['dashboard__credit-score']}
-            agency="experian"
-            logo={experian}
-            maxScore={1200}
-            disabled
-          />
-          <CreditScore
-            className={styles['dashboard__credit-score']}
-            agency="equifax"
-            logo={equifax}
-            maxScore={1200}
-          />
-          <CreditScore
-            className={styles['dashboard__credit-score']}
-            agency="illion"
-            logo={illion}
-            maxScore={1200}
-            disabled
-          />
-        </div>
+      <div className={styles.dashboard__container}>
+        <Text type="h6">Credit Scores</Text>
+        <CreditScore
+          className={styles['dashboard__credit-score']}
+          agency="experian"
+          logo={experian}
+          maxScore={1200}
+          disabled
+        />
+        <CreditScore
+          className={styles['dashboard__credit-score']}
+          agency="equifax"
+          logo={equifax}
+          maxScore={1200}
+        />
+        <CreditScore
+          className={styles['dashboard__credit-score']}
+          agency="illion"
+          logo={illion}
+          maxScore={1200}
+          disabled
+        />
       </div>
     </div>
   );
