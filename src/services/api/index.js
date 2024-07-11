3e2e0e630fcc7f47e5e3ee2cd1bbeecb12b4d77a
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-07-11 11:20:37 +0400

    add an api patch method for the user data changes

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -14,6 +14,7 @@ export const API = {
   refreshToken: data => axiosInstance.post(REFRESH_TOKEN_URL, data),
   getAccount: () => axiosInstance.get('/api/v1/consumers/me'),
   createAccount: data => axiosInstance.post('/api/v1/consumers/me', data),
+  patchAccount: data => axiosInstance.patch('/api/v1/consumers/me', data),
   getId: () => axiosInstance.get('/api/v1/consumers/me/id'),
   addId: data => axiosInstance.post('/api/v1/consumers/me/id', data),
   getLink: () => axiosInstance.get('/api/v1/file-link/consumer.signs'),
