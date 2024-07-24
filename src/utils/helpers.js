19b629f5fbd16aa8a1ca03c260a049de6e7a0e06
jonvardanyan99 <john.vardanyan99@gmail.com>
2024-07-24 10:37:23 +0400

    create a reusable function that returns the objects difference

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
+import isEqual from 'lodash.isequal';
+
 export const dataUrlToFile = async (dataUrl, fileName) => {
   const res = await fetch(dataUrl);
   const blob = await res.blob();
@@ -11,4 +13,14 @@ export const capitalize = word => {
   const result = capitalized + word.slice(1);
 
   return result;
+};
+
+export const diffObjects = (obj1, obj2) => {
+  return Object.keys(obj1).reduce((acc, key) => {
+    if (!isEqual(obj1[key], obj2[key])) {
+      acc[key] = obj1[key];
+    }
+
+    return acc;
+  }, {});
 };
