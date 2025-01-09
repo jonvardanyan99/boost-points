7c7c61bf7904aa03e5735d8439d6dcaed74b1451
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-01-09 15:30:34 +0400

    remove the useMemo and configure the redux logic

--- src/pages/Account/components/Subscription/index.jsx
+++ src/pages/Account/components/Subscription/index.jsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectAccount } from 'store/reducers/user/selectors';
+import { selectAccount } from 'store/slices/user/selectors';
 
 import { NotSubscribed } from './components/NotSubscribed';
 import { Subscribed } from './components/Subscribed';
@@ -8,10 +8,15 @@ import styles from './styles.module.scss';
 
 export const Subscription = () => {
   const account = useSelector(selectAccount);
+  const { subscription } = account;
 
-  const content = useMemo(() => {
-    return account.isSubscriptionActivated ? <Subscribed /> : <NotSubscribed />;
-  }, [account.isSubscriptionActivated]);
-
-  return <div className={styles.subscription}>{content}</div>;
+  return (
+    <div className={styles.subscription}>
+      {subscription.status === 'Active' ? (
+        <Subscribed subscription={subscription} />
+      ) : (
+        <NotSubscribed />
+      )}
+    </div>
+  );
 };
