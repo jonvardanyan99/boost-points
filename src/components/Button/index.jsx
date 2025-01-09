6a2d55e7f19298397e5ee189df26190591ab9a44
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-01-09 14:39:47 +0400

    add option to have an icon and add a transition style

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,24 @@
 import classNames from 'classnames';
 import { Loader } from 'components/Loader';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import styles from './styles.module.scss';
 
-export const Button = ({ className, title, onClick, secondary, loading, disabled }) => {
+export const Button = ({ className, title, onClick, icon, secondary, loading, disabled }) => {
+  const buttonContent = useMemo(() => {
+    if (icon) {
+      return (
+        <>
+          <img src={icon} alt="icon" />
+          {title}
+        </>
+      );
+    }
+
+    return title;
+  }, [title, icon]);
+
   return (
     <button
       type="button"
@@ -21,7 +34,7 @@ export const Button = ({ className, title, onClick, secondary, loading, disabled
       onClick={onClick}
       disabled={disabled || loading}
     >
-      {loading ? <Loader secondary={secondary} /> : title}
+      {loading ? <Loader secondary={secondary} /> : buttonContent}
     </button>
   );
 };
@@ -30,6 +43,7 @@ Button.propTypes = {
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  icon: PropTypes.string,
   secondary: PropTypes.bool,
   loading: PropTypes.bool,
   disabled: PropTypes.bool,
