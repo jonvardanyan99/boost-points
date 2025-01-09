b083b37166cbcd82cff9301924455b54fcd621e4
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-01-09 15:23:21 +0400

    add search params logic to the page and configure

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,26 +1,49 @@
 import classNames from 'classnames';
 import { Text } from 'components/Text';
-import React, { useState } from 'react';
+import { SEARCH_PARAMS } from 'constants/routes';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { capitalize } from 'utils/helpers';
 
 import { Identification } from './components/Identification';
 import { Settings } from './components/Settings';
 import { Subscription } from './components/Subscription';
 import styles from './styles.module.scss';
 
-const tabs = ['Settings', 'Identification', 'Subscription'];
+const tabs = ['settings', 'identification', 'subscription'];
 
 export const Account = () => {
-  const [activeTab, setActiveTab] = useState(tabs[0]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const tabParam = searchParams.get(SEARCH_PARAMS.TAB);
+  const [activeTab, setActiveTab] = useState(tabParam);
 
-  let content;
+  useEffect(() => {
+    setSearchParams({ [SEARCH_PARAMS.TAB]: activeTab });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeTab]);
 
-  if (activeTab === tabs[0]) {
-    content = <Settings />;
-  } else if (activeTab === tabs[1]) {
-    content = <Identification />;
-  } else if (activeTab === tabs[2]) {
-    content = <Subscription />;
-  }
+  useEffect(() => {
+    if (tabParam !== tabs[0] && tabParam !== tabs[1] && tabParam !== tabs[2]) {
+      setSearchParams({ [SEARCH_PARAMS.TAB]: tabs[0] });
+      setActiveTab(tabs[0]);
+    }
+  }, [setSearchParams, tabParam]);
+
+  const content = useMemo(() => {
+    if (tabParam === tabs[0]) {
+      return <Settings />;
+    }
+
+    if (tabParam === tabs[1]) {
+      return <Identification />;
+    }
+
+    if (tabParam === tabs[2]) {
+      return <Subscription />;
+    }
+
+    return null;
+  }, [tabParam]);
 
   return (
     <div className={styles.account}>
@@ -44,12 +67,12 @@ export const Account = () => {
                   [styles['account__tab-text--active']]: activeTab === tab,
                 })}
               >
-                {tab}
+                {capitalize(tab)}
               </Text>
             </button>
           ))}
         </div>
-        {content}
+        <div className={styles['account__content-container']}>{content}</div>
       </div>
     </div>
   );
