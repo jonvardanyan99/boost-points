c1de030dce70be3b9281c0708af484a6c284327c
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-01-10 13:39:26 +0400

    integrate the Stripe into the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.scss';
 
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'router';
 import { persistor, store } from 'store';
 
+const stripePromise = loadStripe(
+  'pk_test_51OLiWAAzcYPZFg3zOfSieeqmNcHnBYAtOCI7yoxn9NPVG1fVXnY8VwfUROo4zkAruCh6oBksb3G2KiIJQtCG7h34004IKS9xLV',
+);
+
 export const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <Router />
+        <Elements stripe={stripePromise}>
+          <Router />
+        </Elements>
       </PersistGate>
     </Provider>
   );
