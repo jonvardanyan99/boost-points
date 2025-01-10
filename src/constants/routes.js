825e1f590883c1c5c7483ede705ad48bb35eafa4
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-01-10 11:33:38 +0400

    add new routes for dispute related pages, for the FAQ and Payment Method pages and add tab search param

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,3 +1,5 @@
+const DISPUTE_DETAILS = '/disputes/:uuid';
+
 export const ROUTES = {
   HOME: '/',
   LOGIN: '/login',
@@ -8,5 +10,17 @@ export const ROUTES = {
   DASHBOARD: '/dashboard',
   REPORT: '/report/:agency',
   SUBSCRIPTION_PLANS: '/subscription-plans',
+  FAQ: '/faq',
   ACCOUNT: '/account',
+  DISPUTE: '/dispute/:uuid',
+  DISPUTES: '/disputes',
+  DISPUTE_DETAILS,
+  UPDATE_DISPUTE: `${DISPUTE_DETAILS}/additional-info`,
+  DISPUTE_FURTHER: `${DISPUTE_DETAILS}/further-action`,
+  ESCALATE_DISPUTE: `${DISPUTE_DETAILS}/contact-support`,
+  PAYMENT_METHOD: '/payment-method',
+};
+
+export const SEARCH_PARAMS = {
+  TAB: 'tab',
 };
