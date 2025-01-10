4d38fa81f3ee33a3c578720c79c5f2f4c0aa1df7
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-01-10 11:42:12 +0400

    create a page to display the frequently asked questions

---
+++ src/pages/FAQ/data/index.js
@@ -0,0 +1,62 @@
+export const questions = [
+  {
+    id: 1,
+    question: 'Getting started',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 2,
+    question: 'How to get reports?',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 3,
+    question: 'Required data for reports',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 4,
+    question: 'Available credit agencies',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 5,
+    question: 'How the scoring system works?',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 6,
+    question: 'What data is involved?',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 7,
+    question: 'Where to look for issues?',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 8,
+    question: 'How to dispute an issue?',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 9,
+    question: 'How to improve my scores?',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 10,
+    question: 'Track your disputes',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 11,
+    question: 'Differences between credit agencies',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+  {
+    id: 12,
+    question: 'How to get an updated report?',
+    text: 'Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus. Lorem ipsum dolor sit amet consectetur. Mauris dolor est enim sit purus aliquet metus.',
+  },
+];
