8be8f773230137e786bc6fd56f2620d40d82b237
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-02-03 11:23:25 +0400

    install @eslint/js typescript-eslint packages and add eslint configure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,33 @@
-module.exports = {
-  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
-  settings: {
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx'],
-      },
-    },
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
+
+export default tseslint.config({
+  files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+  ],
+  plugins: {
+    prettier: prettierPlugin,
+    import: importPlugin,
+    'simple-import-sort': simpleImportSortPlugin,
+    react: reactPlugin,
+    'react-hooks': reactHooksPlugin,
+    'jsx-a11y': jsxA11yPlugin,
   },
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  plugins: ['simple-import-sort', 'prettier'],
   rules: {
+    ...jsxA11yPlugin.configs.recommended.rules,
+    ...reactPlugin.configs.recommended.rules,
+    ...reactHooksPlugin.configs.recommended.rules,
+    ...prettier.rules,
     'no-plusplus': 'off',
     'arrow-body-style': 'off',
 
@@ -68,4 +83,4 @@ module.exports = {
 
     'no-console': 'error',
   },
-};
+});
