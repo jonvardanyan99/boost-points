9090cacf9d22c378d0469e9ae048bf2c3859c226
jonvardanyan99 <john.vardanyan99@gmail.com>
2025-05-20 10:13:52 +0400

    Implement home routing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,9 @@ import close from 'assets/icons/close-white.svg';
 import menu from 'assets/icons/menu.svg';
 import logo from 'assets/images/logo-white.svg';
 import classNames from 'classnames';
+import { ROUTES } from 'constants/routes';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
@@ -15,7 +17,9 @@ interface Props {
 export const Header: React.FC<Props> = ({ icon, onClick, className }) => {
   return (
     <header className={classNames(styles.header, className)}>
-      <img src={logo} alt="logo" />
+      <Link to={ROUTES.HOME} className={styles['header__logo-wrapper']}>
+        <img src={logo} alt="logo" />
+      </Link>
       <button type="button" className={styles['header__icon-wrapper']} onClick={onClick}>
         <img src={icon === 'menu' ? menu : close} alt="menu-icon" />
       </button>
